b7b7f7c93b99d235e88acf34045aa8a7520c62a6
MaXIDoGG <maks.nasonov.03@gmail.com>
2025-04-01 15:45:10 +0700

    first commit

---
+++ definitions.ts
@@ -0,0 +1,53 @@
+const telegramColor = "#0088cc";
+
+// definitions
+Blockly.Blocks['test_block'] = {
+	init: function () {
+		this.appendDummyInput().appendField("Test");
+		this.setColour("#ff0000");
+	}
+};
+
+Blockly.Blocks['import_telegram'] = {
+	init: function () {
+		this.appendDummyInput()
+			.appendField("import telegram");
+		this.setPreviousStatement(true, null);
+		this.setNextStatement(true, null);
+		this.setColour(telegramColor);
+	}
+};
+
+Blockly.Blocks['telegram_bot_init'] = {
+	init: function () {
+		this.appendDummyInput()
+			.appendField(new Blockly.FieldVariable("bot"), "bot")
+			.appendField(" = telegram.Bot(");
+		this.appendValueInput("token")
+			.setCheck("String");
+		this.appendDummyInput()
+			.appendField(")");
+		this.setInputsInline(true);
+		this.setPreviousStatement(true, null);
+		this.setNextStatement(true, null);
+		this.setColour(telegramColor);
+	}
+};
+
+Blockly.Blocks['telegram_send_message'] = {
+	init: function () {
+		this.appendDummyInput()
+			.appendField(new Blockly.FieldVariable("bot"), "bot")
+			.appendField(".send_message(");
+		this.appendValueInput("chat_id")
+			.setCheck("Number");
+		this.appendValueInput("text")
+			.setCheck("String");
+		this.appendDummyInput()
+			.appendField(")");
+		this.setInputsInline(true);
+		this.setPreviousStatement(true, null);
+		this.setNextStatement(true, null);
+		this.setColour(telegramColor);
+	}
+};
